e503befc1558483f5ccd2da55e042c2aba94bb20
박재현 <pjh1019@kbu.ac.kr>
2025-09-17 15:27:44 +0900

    3주차

---
+++ week03/Sample3-1/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte a;
+        short b;
+        int c;
+        long d;
+
+        float f;
+        double k;
+
+        char ch;
+        final float PI = 3.141592f; //Symbolic constant(기호적 상수)
+
+
+        PI = 3.14f;     // 3.14는 float형 Literal
+
+        a = (byte) 128;     // int형 Literal
+        b = (short) 32768;
+        c = (int) 3L;
+        d = 3;
+
+        f = 31.4f;
+        k = 34.1;
+
+        ch = 'A';
+        ch = "A";
+
+        String name = "A";
+        String test = new String("test");
+
+    }
+}
---
+++ week03/Sample3-2/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int byte1 = 127;
+        int byte2 = 34;
+
+        long result = (byte1 + byte2);
+
+        System.out.printf("%d + %d = %d\n", byte1, byte2, result);
+
+    }
+}

c5655610be5d9e15b5b6fbc828b032a882cc2480
박재현 <pjh1019@kbu.ac.kr>
2025-11-26 15:29:49 +0900

    12주차

---
+++ week12/Sample12-01/src/Compute.java
@@ -0,0 +1,7 @@
+public class Compute {
+    static int add(int first, int second) {
+        int result;
+        result = first + second;
+        return result;
+    }
+}
---
+++ week12/Sample12-01/src/Output.java
@@ -0,0 +1,13 @@
+public class Output {
+    // Method OverLoading (중복 정의)
+
+    static void display(int first, int second, int result) {
+        display();  // call by name
+        System.out.printf("%d + %d = %d\n", first, second, result);
+        display();  // call by name
+    }
+
+    private static void display(){
+        System.out.println("*****************************");
+    }
+}
